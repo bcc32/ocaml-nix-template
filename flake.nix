3507efd7c203ea73aa9b35120beceab205dffd8a
Aaron L. Zeng <me@bcc32.com>
2024-11-22 01:40:28 -0500

    nixfmt

--- flake.nix
+++ flake.nix
@@ -7,12 +7,21 @@
     ocaml-overlays.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ocaml-overlays }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ocaml-overlays,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ ocaml-overlays.overlays.default ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; rec {
+      in
+      with pkgs;
+      rec {
         devShells.default = mkShell {
           inputsFrom = [ packages.default ];
           buildInputs = lib.optional stdenv.isLinux inotify-tools ++ [
@@ -28,11 +37,13 @@
           version = "0.1.0";
           useDune2 = true;
           src = ./.;
-          buildInputs = with ocamlPackages;
-            [
-              # FIXME: Add dependencies
-            ];
-          meta = { homepage = "FIXME"; };
+          buildInputs = with ocamlPackages; [
+            # FIXME: Add dependencies
+          ];
+          meta = {
+            homepage = "FIXME";
+          };
         };
-      });
+      }
+    );
 }
